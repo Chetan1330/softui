f0bf8d978ed52128e9fc290f0c7b4628d0a1cc02
Chetan1330 <41874307+Chetan1330@users.noreply.github.com>
2022-10-29 15:34:34 +0530

    Update urls.py

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path("folder/<int:folderid>/",views.folder, name="folder"),
     path("addFolder/", views.addfolder, name="addfolder"),
+    path("delete/<int:deleteid>/",views.delete, name="delete"),
     # Matches any html file
     re_path(r'^.*\.*', views.pages, name='pages'),
 
